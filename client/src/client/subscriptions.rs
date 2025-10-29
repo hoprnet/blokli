b4d8aa111543ebc0dc64a2109d1b60e850414f92
Lukas <lukas.pohanka@inina.net>
2025-10-29 13:04:49 +0100

    feat: make subscription streams Send (#32)

--- client/src/client/subscriptions.rs
+++ client/src/client/subscriptions.rs
@@ -6,28 +6,34 @@ use crate::api::{internal::*, types::*, *};
 
 impl BlokliSubscriptionClient for BlokliClient {
     #[tracing::instrument(level = "debug", skip(self), fields(?selector))]
-    fn subscribe_channels(&self, selector: Option<ChannelSelector>) -> Result<impl Stream<Item = Result<Channel>>> {
+    fn subscribe_channels(
+        &self,
+        selector: Option<ChannelSelector>,
+    ) -> Result<impl Stream<Item = Result<Channel>> + Send> {
         Ok(self
             .build_subscription_stream(SubscribeChannels::build(ChannelsVariables::from(selector)))?
             .try_filter_map(|item| futures::future::ok(Some(item.channel_updated))))
     }
 
     #[tracing::instrument(level = "debug", skip(self), fields(?selector))]
-    fn subscribe_accounts(&self, selector: Option<AccountSelector>) -> Result<impl Stream<Item = Result<Account>>> {
+    fn subscribe_accounts(
+        &self,
+        selector: Option<AccountSelector>,
+    ) -> Result<impl Stream<Item = Result<Account>> + Send> {
         Ok(self
             .build_subscription_stream(SubscribeAccounts::build(AccountVariables::from(selector)))?
             .try_filter_map(|item| futures::future::ok(Some(item.account_updated))))
     }
 
     #[tracing::instrument(level = "debug", skip(self))]
-    fn subscribe_graph(&self) -> Result<impl Stream<Item = Result<OpenedChannelsGraphEntry>>> {
+    fn subscribe_graph(&self) -> Result<impl Stream<Item = Result<OpenedChannelsGraphEntry>> + Send> {
         Ok(self
             .build_subscription_stream(SubscribeGraph::build(()))?
             .try_filter_map(|item| futures::future::ok(Some(item.opened_channel_graph_updated))))
     }
 
     #[tracing::instrument(level = "debug", skip(self))]
-    fn subscribe_transaction(&self, tx_id: TxId) -> Result<impl Stream<Item = Result<Transaction>>> {
+    fn subscribe_transaction(&self, tx_id: TxId) -> Result<impl Stream<Item = Result<Transaction>> + Send> {
         let mut was_confirmed = false;
         Ok(self
             .build_subscription_stream(SubscribeTransaction::build(TransactionsVariables { id: tx_id.into() }))?
