6fcbc9631b2beed0665c6915360ad692d5d56952
Lukas <lukas.pohanka@inina.net>
2025-10-30 16:36:17 +0100

    feat: update tracking API (#34)

--- client/src/client/subscriptions.rs
+++ client/src/client/subscriptions.rs
@@ -31,20 +31,4 @@ impl BlokliSubscriptionClient for BlokliClient {
             .build_subscription_stream(SubscribeGraph::build(()))?
             .try_filter_map(|item| futures::future::ok(Some(item.opened_channel_graph_updated))))
     }
-
-    #[tracing::instrument(level = "debug", skip(self))]
-    fn subscribe_transaction(&self, tx_id: TxId) -> Result<impl Stream<Item = Result<Transaction>> + Send> {
-        let mut was_confirmed = false;
-        Ok(self
-            .build_subscription_stream(SubscribeTransaction::build(TransactionsVariables { id: tx_id.into() }))?
-            .try_filter_map(|item| futures::future::ok(Some(item.transaction_updated)))
-            .try_take_while(move |item| {
-                if was_confirmed {
-                    futures::future::ok(false)
-                } else {
-                    was_confirmed = item.status == TransactionStatus::Confirmed;
-                    futures::future::ok(true)
-                }
-            }))
-    }
 }
