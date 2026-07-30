7a954b9e5f6f5b56a251fac5a929207d49e464d4
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2026-07-30 14:06:40 +0200

    chore: use tag for nix-lib version

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # HOPR Nix Library (provides flake-utils and reusable build functions)
-    nix-lib.url = "github:hoprnet/nix-lib/17ed79793f2e86570f9619555435d861473a5d91";
+    nix-lib.url = "github:hoprnet/nix-lib/v1.3.0";
 
     # Rust build system
     crane.url = "github:ipetkov/crane";
